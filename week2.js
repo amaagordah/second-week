7157d5506e9cabcfcb978ecd0c0b633b472c2b2e
woena agordah <woenaagordah@gmail.com>
2020-05-04 15:44:48 +0000

    secondweek commit

---
+++ week2.js
@@ -0,0 +1,14 @@
+function countdown(){
+    for (var x=0; x<=15; x++) {
+        if (x === 0) {
+                alert(x +  " is even");
+        }
+        else if (x % 2 === 0) {
+                alert(x + " is even");   
+        }
+        else {
+                alert(x + " is odd");
+        }
+}
+    
+}
